a29e4d93cca19364518accceced71af44f4c2068
UcarAli <57497023+UcarAli@users.noreply.github.com>
2022-04-04 21:49:58 +0200

    Update age.java

--- src/age.java
+++ src/age.java
@@ -41,7 +41,6 @@ public class age {
     public static void main(String[] args) {
         int age = 25; //burasi tam olmayabilir
 
-
         if (age<=10 && age>0){
             System.out.println(" facebook hesabi acmak icin cok gencsin");
         } else if (age<=16){
@@ -54,41 +53,6 @@ public class age {
             System.out.println("istedigini yapabilirsin");
         }
 
-
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
